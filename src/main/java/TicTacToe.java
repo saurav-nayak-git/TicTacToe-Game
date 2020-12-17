33e47a3c52c3fcddfefd615112ae121fb08b4348
Saurav Nayak <saurav.nayak65@gmail.com>
2020-12-18 00:05:55 +0530

    Taking Player input and putting it on GameBoard

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -16,6 +16,15 @@ class TicTacToe {
 
         createGameBoard(gameBoard);
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your position (1-9):");
+        int pos = scan.nextInt();
+
+        System.out.println(pos);
+
+
+
+        createGameBoard(gameBoard);
     }
 
     public static void createGameBoard(char[][] gameBoard) {
@@ -24,6 +33,41 @@ class TicTacToe {
                 System.out.print(c);
             }
             System.out.println();
+        }
+    }
+
+    public static void placePiece(char[][] gameBoard, int pos, String user){
+
+         switch (pos) {
+            case 1:
+                gameBoard[0][0] = 'X';
+                break;
+            case 2:
+                gameBoard[0][2] = 'X';
+                break;
+            case 3:
+                gameBoard[0][4] = 'X';
+                break;
+            case 4:
+                gameBoard[2][0] = 'X';
+                break;
+            case 5:
+                gameBoard[2][2] = 'X';
+                break;
+            case 6:
+                gameBoard[2][4] = 'X';
+                break;
+            case 7:
+                gameBoard[3][0] = 'X';
+                break;
+            case 8:
+                gameBoard[3][2] = 'X';
+                break;
+            case 9:
+                gameBoard[3][4] = 'X';
+                break;
+            default:
+                break;
         }
     }
 }
--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -6,13 +6,28 @@ public class TicTacToeTest {
 
     @Test
     public  void createGameBoard(){
+        char[][] gameBoard = mockGameBoard();
+        TicTacToe.createGameBoard(gameBoard);
+        assertTrue(true);
+    }
+
+
+
+
+    @Test
+    public void takePlayerInput(){
+        char[][] gameBoard = mockGameBoard();
+        TicTacToe.placePiece(gameBoard,1,"saurav");
+        assertEquals(gameBoard[0][0],'X');
+    }
+
+    private char[][] mockGameBoard() {
         char[][] gameBoard = {{' ','|',' ','|',' '},
                 {'-','+','-','+','-'},
                 {' ','|',' ','|',' '},
                 {'-','+','-','+','-'},
                 {' ','|',' ','|',' '}
         };
-        TicTacToe.createGameBoard(gameBoard);
-        assertTrue(true);
+        return gameBoard;
     }
 }
